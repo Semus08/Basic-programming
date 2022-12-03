4e3a47cb0d16d1fc03f29e6a31899b01677267ee
Semus08 <sem.maassen@home.nl>
2022-12-03 21:01:41 +0100

    initial project

---
+++ Basic of code/Basic of code/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Basic_of_code
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            basicCode codingBasics = new basicCode();
+            codingBasics.Intergers();
+
+
+          
+        }
+    }
+}
---
+++ Basic of code/Basic of code/basicCode.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Basic_of_code
+{
+    public class basicCode
+    {
+        private float xy;
+
+        public void Strings()
+        {
+            string Broertje = "Roan";
+            Console.WriteLine(Broertje);
+
+        }
+        public void Intergers()
+        {
+            //add 2 whole numbers and print it to the console
+            int x = 7, y = 69;
+            Console.WriteLine(x + y);
+        }
+        public void Floats()
+        {
+            //divide 2 fractional numbers and send it to the console
+            float  x = 9.7f, y = 2.8f;
+            Console.WriteLine(x / y);
+
+            
+        }
+        public void Bools()
+        {
+
+        }
+        public void Loops()
+        {
+            string roan;
+            int x = 8, y = 6;
+            for (int i = 0; i < x; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+
+    }
+}
